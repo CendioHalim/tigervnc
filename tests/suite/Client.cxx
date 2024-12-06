1794e25e9fd79ea369a7feb3ad691ce5531d3eb3
Adam Halim <adaha@cendio.se>
2024-12-06 08:12:41 +0100

    TEMP: suite: Add client class
    This class is a wrapper for rfb::CConnection and replaces a real
    client-server connection with a fake one that doesn't use a network
    connection.
    
    The Client can consume a DummyOutStream produced by a suite::Server by
    simply copying the OutStream and setting it as its InStream.
    
    This makes it possible to record or orchestrate a VNC session and
    process it in non-real-time.

---
+++ tests/suite/Client.cxx
@@ -0,0 +1,90 @@
+#include "Client.h"
+#include "codec/PPMDecoder.h"
+#include <rfb/CMsgReader.h>
+#include <rfb/CMsgWriter.h>
+
+using namespace suite;
+
+Client::Client(int width, int height, rfb::PixelFormat pf)
+{
+  in = new DummyInStream();
+  out = new DummyOutStream();
+  setStreams(in, out);
+
+  setState(RFBSTATE_NORMAL);
+
+  reader = new rfb::CMsgReader(this,in);
+  writer = new rfb::CMsgWriter(&server, out);
+  setReader(reader);
+  setWriter(writer);
+
+  setPixelFormat(fbPF);
+  pb = new rfb::ManagedPixelBuffer(pf, width, height);
+  server.setDimensions(pb->width(),pb->height());
+  setFramebuffer(pb);
+}
+
+Client::~Client()
+{
+  delete in;
+  delete out;
+  // reader & writer get deleted in superclass
+}
+
+void Client::setInStream(DummyInStream* in)
+{
+  delete this->in;
+  this->in = in;
+  setStreams(this->in, this->out);
+
+  delete reader;
+  reader = new rfb::CMsgReader(this, in);
+  setReader(reader);
+}
+
+void Client::copyOutStreamToInStream(DummyOutStream* out)
+{
+  uint8_t* data = new uint8_t[out->length()];
+  memcpy(data, out->dump(), out->length());
+  DummyInStream* is = new DummyInStream(data, out->length());
+  setInStream(is);
+}
+
+
+void Client::resizeFramebuffer()
+{
+  rfb::ModifiablePixelBuffer *pb;
+  pb = new rfb::ManagedPixelBuffer(server.pf(), server.width(),
+                                   server.height());
+  setFramebuffer(pb);
+}
+
+void Client::framebufferUpdateStart()
+{
+  rfb::CConnection::framebufferUpdateStart();
+}
+
+void Client::framebufferUpdateEnd()
+{
+#ifdef _DEBUG
+  const std::string DEBUG_OUTPUT = "debug_output.ppm";
+
+  // Dump framebuffer to file after each framebufferUpdate
+  rfb::PixelBuffer* pb;
+  int stride;
+  pb = getFramebuffer();
+
+  const uint8_t* data = pb->getBuffer(pb->getRect(), &stride);
+
+  PPMDecoder decoder = PPMDecoder();
+  decoder.encodeImageTofile(data, pb->width(), pb->height(),
+                            DEBUG_OUTPUT);
+#endif // _DEBUG
+
+  rfb::CConnection::framebufferUpdateEnd();
+}
+
+bool Client::dataRect(const rfb::Rect&r , int encoding)
+{
+  return rfb::CConnection::dataRect(r, encoding);
+}
---
+++ tests/suite/Client.h
@@ -0,0 +1,44 @@
+#ifndef __SUITE_CLIENT_H__
+#define __SUITE_CLIENT_H__
+
+#include <rfb/CConnection.h>
+#include "Server.h"
+
+namespace suite {
+  class Client : public rfb::CConnection
+  {
+  public:
+    Client(int width, int height, rfb::PixelFormat pf = fbPF);
+    ~Client();
+
+    // Sets InStream and sets a new CMsgReader with the new InStream
+    void setInStream(DummyInStream* in);
+
+    // Creates copy of buffer in OutStream and creates a new InStream
+    // with copied data
+    void copyOutStreamToInStream(DummyOutStream* out);
+
+    virtual void resizeFramebuffer();
+    virtual void framebufferUpdateStart();
+    virtual void framebufferUpdateEnd();
+    virtual bool dataRect(const rfb::Rect&, int);
+
+    // Unused
+    virtual void initDone() {};
+    virtual void setCursor(int, int, const rfb::Point&, const uint8_t*) {};
+    virtual void setCursorPos(const rfb::Point&) {};
+    virtual void setColourMapEntries(int, int, uint16_t*) {};
+    virtual void bell() {};
+
+    rfb::SimpleUpdateTracker updates;
+  protected:
+    rfb::ManagedPixelBuffer* pb;
+  private:
+    DummyInStream *in;
+    DummyOutStream *out;
+    rfb::CMsgReader* reader;
+    rfb::CMsgWriter* writer;
+  };
+}
+
+#endif // __SUITE_CLIENT_H__
